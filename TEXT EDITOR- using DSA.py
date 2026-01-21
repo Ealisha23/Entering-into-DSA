4dfcbcd3e27b3a110a8d42424bfebf38c073c14f
Ealisha Narware <ealishanarware29@gmail.com>
2026-01-21 14:34:36 +0530

    Initial commit: text editor using linked list

---
+++ TEXT EDITOR- using DSA.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+ # Inserting a word at beginning
+
+    def insert_at_beg(self,word):
+        temp = Node(word)
+        temp.next= self.head
+        self.head = temp
+
+    # Undo → delete last nodeclass TextEditor:
+    def __init__(self):
+        self.head = None
+       
+
+    # Typing a word → insert at end
+    def insert_at_end(self, word):
+        new_node = Node(word)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+    def undo(self):
+        if self.head is None:
+            print("Nothing to undo")
+            return
+
+        # If only one word exists
+        if self.head.next is None:
+            self.head = None
+            return
+
+        temp = self.head
+        while temp.next.next:
+            temp = temp.next
+
+        temp.next = None
+
+    # Display current text
+    def display(self):
+        temp = self.head
+        text = []
+
+        while temp:
+            text.append(temp.data)
+            temp = temp.next
+
+        print(" ".join(text))
+
+editor = TextEditor()
+
+editor.insertAtEnd("I am ")
+editor.insertAtEnd("Ealisha")
+editor.insertAtEnd("Narware")
+editor.InsertAtBeg("Hello")
+editor.display()
+# Output: Hello I am  Ealisha Narware
+
+editor.undo()
+editor.display()
+# Output: Hello World
+
+editor.undo()
+editor.display()
+# Output: Hello
+
+editor.undo()
+editor.display()
+# Output: (empty)
+
+    
